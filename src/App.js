463b9322abf72770e54ad777e9ef379daf3de96c
TKG-happy <73483449+TKG-happy@users.noreply.github.com>
2020-12-31 21:27:37 +0900

    Finished Section3-42

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'A', age: 10},
+      {name: 'B', age: 20},
+      {name: 'C', age: 30}
+    ]
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {name: 'AA', age: 101},
+        {name: 'B', age: 202},
+        {name: 'CC', age: 30}
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
+        <p>Hello</p>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
